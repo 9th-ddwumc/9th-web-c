d31864e03bb9cd9f635ef51dd28330e37fd8f67c
heewon-hong <ghdgmldnjs0521@gmail.com>
2025-10-02 02:25:11 +0900

    mission02

---
+++ week3/mission02/src/components/MovieList.tsx
@@ -0,0 +1,16 @@
+import type { Movie } from "../types/movie";
+import MovieCard from "./MovieCard";
+
+interface MovieListProps {
+  movies: Movie[];
+}
+
+export default function MovieList({ movies }: MovieListProps) {
+  return (
+    <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+}
---
+++ week3/mission02/src/pages/MoviePage.tsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import type { Movie, MovieResponse } from "../types/movie";
+import MovieCard from "../components/MovieCard";
+import { LoadingSpinner } from "../components/LoadingSpinner";
+import { useParams } from "react-router-dom";
+import Pagination from "../components/Pagination";
+import MovieList from "../components/MovieList";
+
+
+
+
+export default function MoviesPage () {
+    const [movies,setMovies] = useState<Movie[]>([]);
+    const[isPending, setIsPending] = useState(false);
+    const[isError, setIsError] = useState(false);
+    const [page, setPage] = useState(1);
+
+    const {category}=useParams<{
+        category:string;
+    }>();
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            setIsPending(true);
+            try{
+                const {data} = await axios.get<MovieResponse>(
+                `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=${page}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+                    },
+                }
+                );
+                setMovies(data.results);
+            }catch{
+                setIsError(true);
+            }finally{
+                setIsPending(false);
+            }
+        };
+        fetchMovies();
+     
+    }, [page, category]);
+
+    if(isError){
+        return <div>
+            <span className="text-red-500 font-2xl">에러가 발생햇습니다.</span>
+        </div>
+    }
+
+    return (
+        <>
+        <Pagination
+            page={page}
+            onPrev={() => setPage((prev) => prev - 1)}
+            onNext={() => setPage((prev) => prev + 1)}
+        />
+
+        {isPending ? (
+            <div className="flex items-center justify-center h-dvh">
+            <LoadingSpinner />
+            </div>
+        ) : (
+            <MovieList movies={movies} />
+        )}
+        </>
+    );
+}
