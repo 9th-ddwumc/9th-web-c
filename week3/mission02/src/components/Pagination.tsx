588477b1b8088cb8e9cd26a51dac4e5cad6f3194
heewon-hong <ghdgmldnjs0521@gmail.com>
2025-10-03 04:29:32 +0900

    Update Pagination.tsx

--- week3/mission02/src/components/Pagination.tsx
+++ week3/mission02/src/components/Pagination.tsx
@@ -1,25 +1,30 @@
 interface PaginationProps {
-    page:number;
+    page: number;
+    maxPage: number; // 추가: 총 페이지 수
     onPrev: () => void;
     onNext: () => void;
 }
 
-export default function Pagination({page, onPrev, onNext}: PaginationProps){
-    return(
+export default function Pagination({ page, maxPage, onPrev, onNext }: PaginationProps) {
+    const baseBtnClass = 'bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md hover:bg-[#b2dab1] transition-all duration-200 cursor-pointer disabled:bg-gray-300 disabled:cursor-not-allowed';
+
+    return (
         <div className='flex items-center justify-center gap-6 mt-5'>
-                       <button 
-                           className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md 
-                           hover:bg-[#b2dab1] transition-all duration-200 disabled:bg-gray-300
-                           cursor-pointeer disabled:cursor-not-allowed'
-                           disabled={page ===1} 
-                           onClick={onPrev}>{`<`}
-                       </button>
-                       <span>{page} 페이지</span>
-                       <button 
-                           className='bg-[#dda5e3] text-white px-6 py-3 rounded-lg shadow-md 
-                           hover:bg-[#b2dab1] transition-all duration-200 cursor-pointer'
-                           onClick={onNext}>{`>`}
-                       </button>
-                   </div>
+            <button 
+                className={baseBtnClass}
+                disabled={page === 1} 
+                onClick={onPrev}>
+                {`<`}
+            </button>
+
+            <span>{page} 페이지</span>
+
+            <button 
+                className={baseBtnClass}
+                disabled={page === maxPage}
+                onClick={onNext}>
+                {`>`}
+            </button>
+        </div>
     )
 }
