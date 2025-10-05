6c58e7a5b7345fc4d48f4146ca3bdc12986415f0
minetoiles <byeol9253@gmail.com>
2025-10-05 22:32:31 +0900

    ✨ ui 개선

--- week3/mission2/src/types/movieDetail.ts
+++ week3/mission2/src/types/movieDetail.ts
@@ -30,3 +30,38 @@ export type MovieDetail = {
     vote_average: number;
     vote_count: number;
 }
+
+export interface Cast {
+  adult: boolean;
+  gender: number | null;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+
+export interface Crew {
+  adult: boolean;
+  gender: number | null;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  credit_id: string;
+  department: string;
+  job: string;
+}
+
+export interface MovieCreditsResponse {
+  id: number;
+  cast: Cast[];
+  crew: Crew[];
+}
