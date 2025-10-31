130df45037d2c21e726cc6c25ae40ed5108fdd3b
minetoiles <163957799+minetoiles@users.noreply.github.com>
2025-10-31 15:50:34 +0900

    Delete src directory

--- src/constants/key.ts
+++
@@ -1,4 +0,0 @@
-export const LOCALSTORAGE_KEY = {
-    accessToken: "accessToken",
-    refreshToken: "refreshToken"
-}
--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportantMetaEnv {
-    readonly VITE_SERVER_API_URL: string;
-}
-
-interface ImportantMeta {
-    readonly env: ImportantMetaEnv;
-}
