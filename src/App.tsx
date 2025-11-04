4f931a00ddf6525eafcf29ad967777ff19668b0e
hello-ziwon <zwon0125@gmail.com>
2025-11-04 17:58:14 +0900

    feat: practice completed!

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WelcomeData } from './components/UserDataDisplay';
+
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WelcomeData />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
